7b6562fef4d756040460f20c65f67a23cb467805
Viktor Zhukov <v_zhukov1985@mail.ru>
2019-12-11 23:59:46 +0600

    Add project files.

---
+++ Drum_Led_Controller_Firmware/Drum_Led_Controller_Firmware.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Drum_Led_Controller_Firmware.ino
+ Created:	12/11/2019 11:58:51 PM
+ Author:	Виктор Жуков
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
